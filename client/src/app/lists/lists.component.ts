72253b61c75537cf4eb1820645c56a5ff904c2e5
shivamsahni <shivamsahniss21@gmail.com>
2021-03-20 13:05:51 +0530

    ExtendedApi,used repository design pattern & more

--- client/src/app/lists/lists.component.ts
+++ client/src/app/lists/lists.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListsComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
+  }
 
   ngOnInit(): void {
   }
